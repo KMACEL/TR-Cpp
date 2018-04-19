db3f04f121f52a7973850a64b1b917f473cf605a
Mert Acel <mertacel@gmail.com>
2018-04-19 22:09:29 +0300

    Operator Yukleme

---
+++ 26_OperatorAsiriYukleme/src/main.cpp
@@ -0,0 +1,39 @@
+//============================================================================
+// İsim        : 22_Pointer
+// Yazan       : Mert AceL
+// Version     : 1.0
+// Copyright   : AceL
+// Açıklama    :22_Pointer
+//============================================================================
+#include<iostream>
+using namespace std;
+
+class Asiri
+{
+public:
+    int toplam,deger;
+
+    Asiri operator+(const Asiri &test)
+    {
+        Asiri asiri;
+        asiri.toplam= test.deger*3;
+        return asiri;
+    }
+
+    void yaz(Asiri a)
+    {
+        cout << "Deger : " << a.toplam << "  :  " << a.deger << endl;
+    }
+
+};
+
+int main ()
+{
+    Asiri a;
+    a.deger=5;
+
+    a=a+a;
+    a.yaz(a);
+
+    return 0;
+}
---
+++ 27_OperatorleriAsiriYukleme2/src/main.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// İsim        : 22_Pointer
+// Yazan       : Mert AceL
+// Version     : 1.0
+// Copyright   : AceL
+// Açıklama    :22_Pointer
+//============================================================================
+#include<iostream>
+#include"telefon.h"
+using namespace std;
+
+
+int main ()
+{
+    Telefon tel;
+
+    cin >> tel;
+    cout << tel << endl;
+    return 0;
+}
