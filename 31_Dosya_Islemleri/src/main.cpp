c44dbea06b0c7fcda821e1be2c05cf6f008fa595
Mert Acel <mertacel@gmail.com>
2018-05-03 22:51:25 +0300

    Dosya Islemleri

--- 31_Dosya_Islemleri/src/main.cpp
+++ 31_Dosya_Islemleri/src/main.cpp
@@ -52,7 +52,7 @@ int main ()
     }
 
     string dosya;
-    long konum;
+    //long konum;
     dosyaOku.seekg(3); // satır
     while(dosyaOku >> dosya)
     {
@@ -63,9 +63,111 @@ int main ()
 
     //---------------------------------------------------------------
 
-    
+    cout << "---------------------------------------------------------------" << endl;
+    string dosyaIsmi = "varolan.txt";
+    ifstream dosyaOkumakIcinAc;
+
+    dosyaOkumakIcinAc.open(dosyaIsmi);
+
+    if(dosyaOkumakIcinAc.is_open())
+    {
+        string satir;
+        while(!dosyaOkumakIcinAc.eof())
+        {
+            getline(dosyaOkumakIcinAc,satir);
+            cout << satir << endl;
+        }
+    }
+    else
+    {
+        cout << "Dosya Açılamadı : " << dosyaIsmi << endl;
+        return 1;
+    }
+
+    cout << "---------------------------------------------------------------" << endl;
+
+    string dosyaIsmi2 = "verili.txt";
+    ifstream dosyaOkumakIcinAc2;
+
+    dosyaOkumakIcinAc2.open(dosyaIsmi2);
+
+    if(dosyaOkumakIcinAc2.is_open())
+    {
+        string satir2;
+        string satirSon;
+        while(dosyaOkumakIcinAc2)
+        {
+            getline(dosyaOkumakIcinAc2,satir2,':');
+            dosyaOkumakIcinAc2 >> satirSon;
+            dosyaOkumakIcinAc2 >> ws;
+            if(!dosyaOkumakIcinAc2)
+            {
+                break;
+            }
+
+            cout << satir2 << " - " << satirSon << endl;
+        }
+    }
+    else
+    {
+        cout << "Dosya Açılamadı : " << dosyaIsmi2 << endl;
+        return 1;
+    }
+    cout << "---------------------------------------------------------------" << endl;
+
+    //---------------------------------------------------------------
+
+    struct Insan
+    {
+        string isim;
+        string soyisim;
+        int yas;
+    };
+
+    Insan insan={"Mert","Acel",24};
+    cout << insan.isim << " - " << insan.soyisim << " - " << insan.yas << endl;
+
+    string dosyaIsmiBinary = "binary.bin";
+    ofstream yazBinary;
+
+    yazBinary.open(dosyaIsmiBinary, ios::binary);
+
+    if(yazBinary.is_open())
+    {
+        //yazBinary.write((char *)&insan,sizeof(Insan));
+        yazBinary.write(reinterpret_cast<char *>(&insan),sizeof(Insan));
+
+        yazBinary.close();
+    }
+    else
+    {
+        cout << "Dosya Oluşturulamadı : " << dosyaIsmiBinary << endl;
+    }
+
+    //---------------------------------------------------------------
+    cout << "---------------------------------------------------------------" << endl;
+
+    Insan insan2={};
+    string dosyaIsmiBinary2 = "binary.bin";
+    ifstream okuBinary;
+
+    okuBinary.open(dosyaIsmiBinary2, ios::binary);
+
+    if(okuBinary.is_open())
+    {
+        okuBinary.read(reinterpret_cast<char *>(&insan2),sizeof(Insan));
+        okuBinary.close();
+    }
+    else
+    {
+        cout << "Dosya Okunamadı : " << dosyaIsmiBinary2 << endl;
+    }
+    cout << "Okunan : " << insan2.isim << " - " << insan2.soyisim << " - " << insan2.yas << endl;
+
     return 0;
 }
+
+
 
 // ios::app - Her veriyi sonuna ekler
 // ios::out - Yoksa yaratır verileri ekler, varsa dosya iini siler
