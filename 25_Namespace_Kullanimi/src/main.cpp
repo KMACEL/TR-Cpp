2ccb7f4efc9bb3b98caf4be19d414459effb2cb4
Mert Acel <mertacel@gmail.com>
2018-05-18 23:36:55 +0300

    Namespace Eklendi, Thread Eklendi

---
+++ 25_Namespace_Kullanimi/src/main.cpp
@@ -0,0 +1,71 @@
+//============================================================================
+// İsim        : 25_Namespace_Kullanimi
+// Yazan       : Mert AceL
+// Version     : 1.0
+// Copyright   : AceL
+// Açıklama    : 25_Namespace_Kullanimi
+//============================================================================
+#include<iostream>
+#include<string>
+using namespace std;
+
+namespace ilk
+{
+    string testVar;
+
+    void yazdir()
+    {
+        cout << "Bu ilk Namespacetir " << testVar << endl;
+    }
+
+    class TestClass
+    {
+    public:
+        TestClass()
+        {
+            cout << "ilk içinde TestClass oluşutuldu" << endl;
+        }
+    };
+}
+
+namespace ikinci
+{
+    int testVar;
+
+    void yazdir()
+    {
+        cout << "Bu ikinci Namespacetir " << testVar << endl;
+    }
+
+    class TestClass
+    {
+    public:
+        TestClass()
+        {
+            cout << "ikinci içinde TestClass oluşutuldu" << endl;
+        }
+
+        void mesaj() {
+            /* code */
+            cout << "ikinci mesajjj" << endl;
+        }
+    };
+}
+
+int main ()
+{
+    ilk::testVar = "haha";
+    ilk::yazdir();
+    ilk::TestClass test;
+
+    ikinci::testVar = 5;
+    ikinci::yazdir();
+    ikinci::TestClass test2;
+    test2.mesaj();
+
+    //------------------------------------------------------------
+
+    using ilk::TestClass;
+    TestClass t;
+    return 0;
+}
---
+++ 99_Thread_Kullanımı/src/main.cpp
@@ -0,0 +1,45 @@
+//============================================================================
+// İsim        : 25_Namespace_Kullanimi
+// Yazan       : Mert AceL
+// Version     : 1.0
+// Copyright   : AceL
+// Açıklama    : 25_Namespace_Kullanimi
+//============================================================================
+#include<iostream>
+#include<string>
+#include<thread>
+#include<chrono>
+using namespace std;
+
+void islem(string mesaj, int zaman)
+{
+    std::this_thread::sleep_for (std::chrono::seconds(zaman));
+    cout << "Mesajınız : " << mesaj << endl;
+}
+
+int main ()
+{
+    cout << "Main Thread ID : " << this_thread::get_id() << endl;
+
+    cout << "Aktif Thread : " << thread::hardware_concurrency() << endl;
+
+    thread t1(islem, "1. Thread", 3);
+    //t1.join();
+    t1.detach();
+    //if(t1.joinable())
+    //   t1.join();
+
+    thread t2(islem, "2. Thread", 1);
+    //t2.join();
+    t2.detach();
+
+    thread t3(islem, "3. Thread", 5);
+    cout << "t3 Thread : " << t3.get_id() << endl;
+    //t3.join();
+    t3.detach();
+
+    string x;
+    cin >> x;
+
+    return 0;
+}
